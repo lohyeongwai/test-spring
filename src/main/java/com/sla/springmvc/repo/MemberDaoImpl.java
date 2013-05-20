85a8e4fc3db39c88a21091e48e1bac9a81f58b4e
lohyeongwai <loh.yeongwai@sla-mobile.com.my>
2013-05-20 17:57:46 +0800

    here is from staged to remote repo

--- src/main/java/com/sla/springmvc/repo/MemberDaoImpl.java
+++ src/main/java/com/sla/springmvc/repo/MemberDaoImpl.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 /*
- * test change again
+ * test sth
  */
 @Repository
 @Transactional
