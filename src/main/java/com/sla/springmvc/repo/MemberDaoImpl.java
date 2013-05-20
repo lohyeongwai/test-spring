21423593e11ef5bb2e38a20490ff80635df9aa68
lohyeongwai <loh.yeongwai@sla-mobile.com.my>
2013-05-20 16:43:44 +0800

    commit 1 file

--- src/main/java/com/sla/springmvc/repo/MemberDaoImpl.java
+++ src/main/java/com/sla/springmvc/repo/MemberDaoImpl.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 /*
- * test
+ * test sth
  */
 @Repository
 @Transactional
